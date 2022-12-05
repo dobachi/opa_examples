edafe5f21abbbdb39ae7885b8a60e09bd949b68f
dobachi <dobachi1983oss@gmail.com>
2022-12-06 00:10:49 +0900

    Change version

--- bin/get_opa.sh
+++ bin/get_opa.sh
@@ -2,6 +2,6 @@
 
 CURRENT=$(cd $(dirname $0);pwd)
 VERSION=v0.46.1
-wget https://github.com/open-policy-agent/opa/releases/download/${VERSION}/opa_linux_amd64 -O ${CURRENT}/opa
+wget https://github.com/open-policy-agent/opa/releases/download/v0.47.0/opa_linux_amd64_static -O ${CURRENT}/opa
 chmod a+x ${CURRENT}/opa
 
